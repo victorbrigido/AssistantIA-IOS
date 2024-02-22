0b298160e9e49444b04930a28dc4e2216271933a
Victor Brigido <victorbrigido@icloud.com>
2024-02-22 09:48:48 -0300

    Initial Commit

---
+++ AssistantIA/AssistantIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistantIAApp.swift
+//  AssistantIA
+//
+//  Created by Victor Brigido on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AssistantIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
